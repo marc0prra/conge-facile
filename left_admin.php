0c81f81ba864cdb5118102198ca947097e3f4c7e
Redwane Kouadria <redwanekouadria@gmail.com>
2025-03-21 14:15:40 +0100

    Administration

---
+++ left_admin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="style.css?v=2" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Epilogue:wght@100;200;300;400;500;600;700;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+      rel="stylesheet"
+    />
+
+    <link
+      href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+    <link rel="icon" type="image/x-icon" href="img/icone.ico" />
+
+    <title>Accueil</title>
+  </head>
+</html>
+
+<body>
+  <div class="left">
+    <a href="accueil.php">Accueil</a>
+    <a href="nouvelle.php">Nouvelle demande</a>
+    <a href="historique.php">Historique des demandes</a>
+    <div class="rod"></div>
+    <a href="infosC.php">Mes informations</a>
+    <a href="preferences.php">Mes préférences</a>
+    <a href="administration.php">Administration</a>
+    <a href="deconnexion.php">Déconnexion</a>
+  </div>
+  <script src="script.js"></script>
+</body>
