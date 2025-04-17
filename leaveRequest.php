12a6de9bd5e089457759283cc21a866b0d9bb737
Valentin <valentin.godard2006@gmail.com>
2025-04-17 16:31:51 +0200

    Update leaveRequest.php

--- leaveRequest.php
+++ leaveRequest.php
@@ -65,7 +65,8 @@ if (!$demande) {
                 <div class="sectionRequestDetails">
                     <p class="TypeRequest"> Type de congé : <?= htmlspecialchars($demande['type_demande']) ?> </p>
                     <p class="TypeRequest"> Période : <?= htmlspecialchars($demande['date_debut']) ?> au
-                        <?= htmlspecialchars($demande['date_fin']) ?> </p>
+                        <?= htmlspecialchars($demande['date_fin']) ?> 
+                    </p>
                     <div class="justificative RequestDetails">
                         <p class="color">Commentaire du manager</p>
                         <input class="managerComment" type="text" name="comment"
