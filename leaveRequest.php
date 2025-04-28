26d9d42be9daf2b037d924622f4d688acc703f88
Valentin <valentin.godard2006@gmail.com>
2025-04-28 11:45:17 +0200

    Update leaveRequest.php

--- leaveRequest.php
+++ leaveRequest.php
@@ -26,7 +26,8 @@ $query = "SELECT
             request.created_at AS date_demande, 
             request.start_at AS date_debut, 
             request.end_at AS date_fin,
-            request.comment AS commentaire
+            request.comment AS commentaire,
+            request.answer AS etat_demande
           FROM request
           JOIN request_type ON request.request_type_id = request_type.id
           WHERE request.id = :request_id";
@@ -61,12 +62,63 @@ if (!$demande) {
                 <h1> Ma demande de congé </h1>
 
                 <?php if ($demande): ?>
-                <p> Demande du <?= htmlspecialchars($demande['date_demande']) ?></p>
+                <p> Demande du <?=htmlspecialchars(date('d/m/Y', strtotime($demande['date_demande']))) ?></p>
+                <?php 
+            $holidays = [
+                "2025-01-01",
+                "2025-04-21",
+                "2025-05-01",
+                "2025-05-08",
+                "2025-05-29",
+                "2025-06-09",
+                "2025-07-14",
+                "2025-08-15",
+                "2025-11-01",
+                "2025-11-11",
+                "2025-12-25"
+            ];
+
+            function getWorkingDays($start, $end, $holidays = []) {
+                $begin = new DateTime($start);
+                $end = new DateTime($end);
+                $end->modify('+1 day');
+
+                $interval = new DateInterval('P1D');
+                $dateRange = new DatePeriod($begin, $interval, $end);
+
+                $workingDays = 0;
+                foreach ($dateRange as $date) {
+                    $day = $date->format('N'); // 6 = samedi, 7 = dimanche
+                    $formatted = $date->format('Y-m-d');
+                        if ($day < 6 && !in_array($formatted, $holidays)) {
+                            $workingDays++;
+                        }
+                }
+                return $workingDays;
+            }
+
+            function getStatus($codes) {
+                // Convertir la chaîne en tableau
+                $numbers = explode(',', $codes);
+            
+                // Vérifier les numéros dans l'ordre de priorité
+                if (in_array('0', $numbers)) {
+                    return 'En cours';
+                } elseif (in_array('1', $numbers)) {
+                    return 'Acceptée';
+                } elseif (in_array('2', $numbers)) {
+                    return 'Refusée';
+                } else {
+                    return 'Statut inconnu';
+                }
+            }
+
+            ?>
                 <div class="sectionRequestDetails">
                     <p class="TypeRequest"> Type de congé : <?= htmlspecialchars($demande['type_demande']) ?> </p>
-                    <p class="TypeRequest"> Période : <?= htmlspecialchars($demande['date_debut']) ?> au
-                        <?= htmlspecialchars($demande['date_fin']) ?> 
-                    </p>
+                    <p class="TypeRequest"> Période : <?= htmlspecialchars((new DateTime($demande['date_debut']))->format('d/m/Y H\h00')) ?> au <?= htmlspecialchars((new DateTime($demande['date_fin']))->format('d/m/Y H\h00')) ?> </p>
+                    <p class="TypeRequest"> Nombre de jours : <?= getWorkingDays($demande['date_debut'], $demande['date_fin'], $holidays); ?></p>
+                    <p class="TypeRequest"> Statut de la demande : <?= getStatus($demande['etat_demande']) ?></p>
                     <div class="justificative RequestDetails">
                         <p class="color">Commentaire du manager</p>
                         <input class="managerComment" type="text" name="comment"
