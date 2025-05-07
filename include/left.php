ab05ab2c1bdcbfa0bbd693eb30e50e2f1b793912
Valentin <valentin.godard2006@gmail.com>
2025-05-07 17:11:47 +0200

    Update left.php

--- include/left.php
+++ include/left.php
@@ -66,7 +66,7 @@ try {
     <div class="collab"></div>
     <a href="accueil.php">Accueil</a>
     <a href="demandInExpectation.php">Demandes en attente <p class="numberRequest"><?= htmlspecialchars($nombreDemandes) ?> </p> </a>
-    <a href="historique.php">Historique des demandes</a>
+    <a href="historyRequest.php">Historique des demandes</a>
     <a href="#">Mon équipe</a>
     <a href="#">Statistiques</a>
     <div class="rod"></div>
